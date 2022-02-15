b05753e8216e37722fbb585e42ce5d97463df2af
Mário Ponye <92544559+MarioPonte@users.noreply.github.com>
2022-02-15 23:26:19 +0000

    Add files via upload

--- js/noticias-script.js
+++ js/noticias-script.js
@@ -44,17 +44,17 @@ async function getDados() {
                 if(dataISODataHora.getHours()<10){
 
                     if(dataISODataHora.getMinutes()<10){
-                        dataFormatada = dataISODataHora.getFullYear() + "-0" + (dataISODataHora.getMonth() + 1) + "-0" + dataISODataHora.getDate() + " | 0" + dataISODataHora.getHours() + ":0" + dataISODataHora.getMinutes();
+                        dataFormatada = dataISODataHora.getFullYear() + "-0" + (dataISODataHora.getMonth() + 1) + "-" + dataISODataHora.getDate() + " | 0" + dataISODataHora.getHours() + ":0" + dataISODataHora.getMinutes();
                     }else{
-                        dataFormatada = dataISODataHora.getFullYear() + "-0" + (dataISODataHora.getMonth() + 1) + "-0" + dataISODataHora.getDate() + " | 0" + dataISODataHora.getHours() + ":" + dataISODataHora.getMinutes();
+                        dataFormatada = dataISODataHora.getFullYear() + "-0" + (dataISODataHora.getMonth() + 1) + "-" + dataISODataHora.getDate() + " | 0" + dataISODataHora.getHours() + ":" + dataISODataHora.getMinutes();
                     }
 
                 }else{
 
                     if(dataISODataHora.getMinutes()<10){
-                        dataFormatada = dataISODataHora.getFullYear() + "-0" + (dataISODataHora.getMonth() + 1) + "-0" + dataISODataHora.getDate() + " | " + dataISODataHora.getHours() + ":0" + dataISODataHora.getMinutes();
+                        dataFormatada = dataISODataHora.getFullYear() + "-0" + (dataISODataHora.getMonth() + 1) + "-" + dataISODataHora.getDate() + " | " + dataISODataHora.getHours() + ":0" + dataISODataHora.getMinutes();
                     }else{
-                        dataFormatada = dataISODataHora.getFullYear() + "-0" + (dataISODataHora.getMonth() + 1) + "-0" + dataISODataHora.getDate() + " | " + dataISODataHora.getHours() + ":" + dataISODataHora.getMinutes();
+                        dataFormatada = dataISODataHora.getFullYear() + "-0" + (dataISODataHora.getMonth() + 1) + "-" + dataISODataHora.getDate() + " | " + dataISODataHora.getHours() + ":" + dataISODataHora.getMinutes();
                     }
 
                 }
