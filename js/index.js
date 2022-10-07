e49c293b6069c1b0a612c53983012973ad14d1c4
Mário Ponte <ponteolavo30@gmail.com>
2022-10-07 19:18:30 +0100

    edit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+if("ServiceWorker" in navigator){
+    navigator.serviceWorker.register("sw.js").then(registration => {
+        console.log("sw registed");
+        console.log(registration);
+    }).catch(error => {
+        console.log("sw registation failed");
+        console.log(error);
+    });
+}else{
+    console.log("no");
+}
