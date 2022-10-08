73e8087b35cb2c85c79ec826bf3909d4b9fb9c10
Mário Ponte <ponteolavo30@gmail.com>
2022-10-08 23:38:36 +0100

    edit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("./sw.js").then(registration => {
+    navigator.serviceWorker.register("sw.js").then(registration => {
         console.log("sw registed");
         console.log(registration);
     }).catch(error => {
