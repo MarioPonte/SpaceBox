0d9bf9abb2f24c962ae342050e68fd259efc9be3
Mário Ponte <ponteolavo30@gmail.com>
2022-10-12 19:41:39 +0100

    edit

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["/.","/images/logo5.png","/images/logo6.png"]);
+            //return cache.addAll(["/.","/images/logo5.png","/images/logo6.png"]);
+            return cache.addAll(["/SpaceBox/.","/SpaceBox/images/logo5.png","/SpaceBox/images/logo6.png"]);
         })
     );
 });
