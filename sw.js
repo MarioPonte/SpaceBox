5a20cf0e2fc9eca449506ab8bc720701cea3536e
Mário Ponte <ponteolavo30@gmail.com>
2022-10-13 15:57:13 +0100

    edit

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["/.","/images/logo5.png","/images/logo6.png"]);
+            return cache.addAll(["/.","/images/logo5.png","/images/logo6.png","/fallback.html","/css/fallback.css"]);
 
             /* COLOCAR DEPOIS DE TODOS OS TESTES ESTAREM CONCLUIDOS
             return cache.addAll(["/SpaceBox/.","/SpaceBox/images/logo5.png","/SpaceBox/images/logo6.png"]);
@@ -16,6 +16,6 @@ self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(response => {
             return response || fetch(e.request);
-        })
+        }).catch(() => caches.match("fallback.html"))
     );
 });
