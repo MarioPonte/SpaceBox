5025ef3ade8cd6a5e1d1b30c807b2e87a03817dc
Mário Ponte <ponteolavo30@gmail.com>
2022-10-14 20:10:29 +0100

    edit

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["/.","/images/logo5.png","/images/logo6.png","/fallback.html","/css/fallback.css"]);
+            return cache.addAll(["/.","/images/logo5.png","/images/logo6.png","/images/offline.png","/fallback.html","/css/fallback.css"]);
 
             /* COLOCAR DEPOIS DE TODOS OS TESTES ESTAREM CONCLUIDOS
             return cache.addAll(["/SpaceBox/.","/SpaceBox/images/logo5.png","/SpaceBox/images/logo6.png"]);
