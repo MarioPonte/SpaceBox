e4d87cc446b9460cef30c9292e4f0ff794f91b39
Mário Ponte <ponteolavo30@gmail.com>
2022-11-06 18:04:10 +0000

    edit

--- js/fotos-marte-script.js
+++ js/fotos-marte-script.js
@@ -1,64 +1,40 @@
-
-// Esta função pega o rover e manda ele para que possa ser chamado
+// This function takes the rover and sends it so it can be called
 function getRover() {
 	return document.getElementById('rover').value;
 }
 
-// Esta função pega a data do campo de procura e manda ela para que possa ser chamada
+// This function takes the date from the search field and sends it so it can be called
 function getDate() {
 	return document.getElementById('dateSearch').value;
 }
 
-// Data atual
-
+// Actual date
 var data = new Date();
 var ano = data.getFullYear();
 var mes = data.getMonth()+1;
 var dia = String(data.getDate()).padStart(2,'0');
 var dataPadrao = '';
 
-if(mes<10){
-	dataPadrao = ano + '-' + '0' + mes + '-' + dia;
-}else{
-	dataPadrao = ano + '-' + mes + '-' + dia;
-}
-
-console.log(dataPadrao);
+if(mes<10) dataPadrao = ano + '-' + '0' + mes + '-' + dia; else dataPadrao = ano + '-' + mes + '-' + dia;
 
 document.getElementById("dateSearch").max = dataPadrao;
 document.getElementById("dateSearch").min = '1995-06-20';
 
-// Função de chamada
-
+// Call Function
 async function call() {
-	
-$('#mars-images').children("a").remove();
-	
 
-// Request api da NASA
+	let queryFull = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + getRover() + "/photos?earth_date=" + getDate() + "&api_key=leWsmyC7lJSYGrpntyDmUmaenjXwR4zZKbPv4LgJ";
 	
-let xmlhttp = new XMLHttpRequest();
-		
-		xmlhttp.onreadystatechange = function () {
-			if (this.readyState == 4 && this.status == 200) {
-				let data = JSON.parse(this.responseText);
-				
-				
-				let photos = data["photos"];
-				
-				data['photos'].forEach(function (d) {
-					$('#mars-images').append('<a href="' + d['img_src'] + '" data-lightbox="photos"><img id="photo" class="photo" src="' + d['img_src'] + '"></a>');
-				});
-				
-			}
-		}
-		
-		let queryRover = getRover();
-		
-		let queryFull = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + queryRover + "/photos?earth_date=" + getDate() + "&api_key=leWsmyC7lJSYGrpntyDmUmaenjXwR4zZKbPv4LgJ";
-		
-		xmlhttp.open("GET", queryFull, true);
-		xmlhttp.send();
+	$('#mars-images').children("a").remove();
+
+	const dados = await fetch(queryFull);
+    const response = await dados.json();
+
+    const pegueiDados = JSON.parse(JSON.stringify(response));
+
+	pegueiDados.photos.forEach(function (d) {
+		$('#mars-images').append('<a href="' + d['img_src'] + '" data-lightbox="photos"><img id="photo" class="photo" src="' + d['img_src'] + '"></a>');
+	});
 
 }
 	
--- sw.js
+++ sw.js
@@ -1,11 +1,8 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["/.","/images/logo5.png","/images/logo6.png","/images/offline.png","/fallback.html","/css/fallback.css"]);
-
-            /* COLOCAR DEPOIS DE TODOS OS TESTES ESTAREM CONCLUIDOS
-            return cache.addAll(["/SpaceBox/.","/SpaceBox/images/logo5.png","/SpaceBox/images/logo6.png"]);
-            */
+            //return cache.addAll(["/.","/images/logo5.png","/images/logo6.png","/images/offline.png","/fallback.html","/css/fallback.css"]);
+            return cache.addAll(["/SpaceBox/.","/SpaceBox/images/logo5.png","/SpaceBox/images/logo6.png","/SpaceBox/images/offline.png","/SpaceBox/fallback.html","/SpaceBox/css/fallback.css"]);
         })
     );
 });
